5a74391d797be0e18ce8fa1a2ba47b0d9bec96cf
Ferhad <farhadsb@code.edu.az>
2023-02-04 12:38:56 +0400

    init

---
+++ Kesr/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kesr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kesr ededi daxil edin-");
+            double kesr = double.Parse(Console.ReadLine());
+
+            //Console.Write("kesr hissede nece eded var-");
+
+            //int kesrsay =int.Parse( Console.ReadLine());
+            //int onluq = (int)Math.Pow(10, kesrsay);
+            double b = kesr % 1;
+
+            int f = kesr.ToString().Split('.')[1].Length;
+            int d= (int) Math.Pow(10, f);
+            int tam = Convert.ToInt32(kesr);
+            int qaliq= Convert.ToInt32(b*d);
+
+            if (b < 0.5)
+
+                Console.WriteLine($"{qaliq}.{tam}");
+
+            else
+                Console.WriteLine($"{qaliq}.{tam - 1}");
+
+            //string w = Convert.ToString(b);
+            //string e = w.Replace("0.", string.Empty);
+            //int f= Convert.ToInt32(e);
+            //double c = b * onluq;
+            //int g = Convert.ToInt32(c);
+            //Console.WriteLine($"{kesrsay}.{h}");
+            //Console.WriteLine(f);
+
+
+
+
+
+
+        }
+    }
+}
